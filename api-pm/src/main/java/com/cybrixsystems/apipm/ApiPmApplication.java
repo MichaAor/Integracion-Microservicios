409bbe52fbd116163997155e925628d1a1dfef82
Bluckiz <57368872+MichaAor@users.noreply.github.com>
2023-06-03 21:38:48 -0300

    Implementacion base de la api para gestion de productos.

---
+++ api-pm/src/main/java/com/cybrixsystems/apipm/ApiPmApplication.java
@@ -0,0 +1,13 @@
+package com.cybrixsystems.apipm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPmApplication.class, args);
+	}
+
+}
---
+++ api-pm/src/test/java/com/cybrixsystems/apipm/ApiPmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybrixsystems.apipm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
