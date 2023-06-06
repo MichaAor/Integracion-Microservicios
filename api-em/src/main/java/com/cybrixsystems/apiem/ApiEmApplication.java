f07f2a32980afee0b812e8090b2c04112565ebaa
MairaVallejo <77942897+MairaVallejo@users.noreply.github.com>
2023-06-06 00:01:40 -0300

    Initial commit

---
+++ api-em/src/main/java/com/cybrixsystems/apiem/ApiEmApplication.java
@@ -0,0 +1,13 @@
+package com.cybrixsystems.apiem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmApplication.class, args);
+	}
+
+}
---
+++ api-em/src/test/java/com/cybrixsystems/apiem/ApiEmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybrixsystems.apiem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
